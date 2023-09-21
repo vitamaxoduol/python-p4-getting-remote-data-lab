39b699719eb5dd5bc24ffaf4e72cd8e913a9908b
Vitamax <vitamaxoduo@gmail.com>
2023-09-21 16:58:48 +0300

    Completed

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -5,9 +5,15 @@ class GetRequester:
 
     def __init__(self, url):
         self.url = url
+        
+        # URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+        
 
     def get_response_body(self):
-        pass
+        
+        response = requests.get(self.url)
+        # response.raise_for_status()
+        return response.content
 
     def load_json(self):
-        pass
+        return json.loads(self.get_response_body())
